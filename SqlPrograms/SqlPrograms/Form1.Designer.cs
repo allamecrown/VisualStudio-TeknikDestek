95a984d7c9cd772b1714276f404bf61e55e181a9
lutfullah azı <30508296+allamecrown@users.noreply.github.com>
2017-07-29 23:36:47 +0300

    First Create

---
+++ SqlPrograms/SqlPrograms/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SqlPrograms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.usernametxtbx = new System.Windows.Forms.TextBox();
+            this.passwordtxtbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(76, 104);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(89, 34);
+            this.loginbtn.TabIndex = 0;
+            this.loginbtn.Text = "Giriş";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // usernametxtbx
+            // 
+            this.usernametxtbx.Location = new System.Drawing.Point(98, 26);
+            this.usernametxtbx.Name = "usernametxtbx";
+            this.usernametxtbx.Size = new System.Drawing.Size(100, 20);
+            this.usernametxtbx.TabIndex = 1;
+            // 
+            // passwordtxtbx
+            // 
+            this.passwordtxtbx.Location = new System.Drawing.Point(98, 52);
+            this.passwordtxtbx.Name = "passwordtxtbx";
+            this.passwordtxtbx.Size = new System.Drawing.Size(100, 20);
+            this.passwordtxtbx.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 170);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordtxtbx);
+            this.Controls.Add(this.usernametxtbx);
+            this.Controls.Add(this.loginbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.TextBox usernametxtbx;
+        private System.Windows.Forms.TextBox passwordtxtbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SqlPrograms/SqlPrograms/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace SqlPrograms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region sql'e baglanti
+        SqlConnection baglantı = new SqlConnection("Data Source = DESKTOP - MSE4Q0T/SQLEXPRESS; Initial Catalog = kisiler; Integrated Security = True"); // Sql connection ile bağlanacağı sql
+                                                                                                                                    // server yolunu gösterdik.
+        #endregion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            #region boşluk kontrolü
+            if (usernametxtbx.Text == "" && passwordtxtbx.Text == "") // Eger usenametxtbx (kullanıcı adı kutusu) yazısı "" bu ise yani hiçbir karakter yok ise,
+                                                                      // Ve passwordtxtbx (şifre kutusu) yazısı "" bu ise yani 
+                                                                      // Hiçbir karakter yok ise, 
+            {
+                MessageBox.Show("Lütfen Kullanıcı Adınızı veya Şifrenizi Boş Bırakmayınız"); // Ekrana "Lütfen Kullanıcı Adınızı veya Şifrenizi Boş Bırakmayınız" mesajını yaz dedik.
+
+            }
+            #endregion
+            #region sqlsorgu
+            else
+            {
+                baglantı.Open(); // Yukarıda ki yolu bağlatıyı aç diyerek etkinleştirdik.
+                SqlCommand komut = new SqlCommand("select * from kisiler where username= '" + usernametxtbx.Text + " ' and password='" + passwordtxtbx.Text + "'", baglantı); // Sql
+                // command komutu ile sql sorgu diline geçiş yaptık ve kullanıcı adı kutusunu ve şifre kutusunu ekleyerek verileri karşılaştırdık. En sonunda bağlantıyı aktif ederek
+                // bağlandık.
+                SqlDataReader dr = komut.ExecuteReader(); // Verilerin hazır olup olmağıgını öğrenmek için dr komutuna yukarıdaki verilerin hazır olup olmadığını gösteren ExecuteReader
+                // komutunu ekledik.
+                if (dr.Read()) // Burada eger dr hazır ise aşağıdaki komutları yap dedik.
+                {
+
+                    MessageBox.Show("Hosgeldiniz");
+                }
+                       else
+                             {
+                              MessageBox.Show("Hatalı Giriş"); // Bukarıdaki değil ise ekrana "Hatalı Giriş"yaz dedik.
+                            }
+
+                baglantı.Close();
+            }
+            #endregion
+        }
+    }
+}
